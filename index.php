83b6925fdfcb4c4a1036180579660f0cdf99bc12
Ousmane Bah <62627612+yzd224@users.noreply.github.com>
2020-05-31 15:32:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 
+<?php
+   define("URL_ASSETS","http://localhost/Examenliage-Quizz/Quizz/assets");
+
+  require_once('./libs/Router.php');
+   $router=new Router();
+   //controller/methode=>UC
+   $router->getRoute();
+   /*
+   $sec=new Security();
+   $sec->showPage();
+   $obj->{$method}()
+   */
+?>
